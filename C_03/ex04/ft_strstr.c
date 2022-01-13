c9230fd1f0fb6d8a9311cd8d9e2e70a1674779f3
33bini <68188768+33bini@users.noreply.github.com>
2022-01-13 21:13:23 +0900

    C_03

---
+++ C_03/ex04/ft_strstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeblee <yeblee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 13:02:20 by yeblee            #+#    #+#             */
+/*   Updated: 2022/01/13 17:24:03 by yeblee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (to_find[len] != '\0')
+		len++;
+	if (len == 0)
+		return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && str[i + j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
