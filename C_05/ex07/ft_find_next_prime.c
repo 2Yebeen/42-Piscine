30bcf837ced82a992c4fafe4fa8d081b069f86aa
33bini <68188768+33bini@users.noreply.github.com>
2022-01-18 02:14:10 +0900

    Update ft_find_next_prime.c

--- C_05/ex07/ft_find_next_prime.c
+++ C_05/ex07/ft_find_next_prime.c
@@ -11,14 +11,29 @@
 /* ************************************************************************** */
 int	is_prime(int nb)
 {
-	
+	int		i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+
 }
 
 int	ft_find_next_prime(int nb)
 {
-	if (ft_is_prime(nb))
-		return (nb);
-	else
-		ft_is_prime(
-	
+	int i;
+
+	if (nb <= 1)
+		return (2);
+	i = nb;
+	while (!ft_is_prime(i))
+		i++;
+	return (i);
 }
