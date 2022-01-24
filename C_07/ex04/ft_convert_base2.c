e8f9895e09265d6f5f87ad06966f5bdb8aa9220d
33bini <68188768+33bini@users.noreply.github.com>
2022-01-25 00:43:31 +0900

    commit

--- C_07/ex04/ft_convert_base2.c
+++ C_07/ex04/ft_convert_base2.c
@@ -12,6 +12,49 @@
 
 #include <unistd.h>
 
+int	ft_strlen(char *str)
+{
+	int	cur;
+
+	cur = 0;
+	while (str[cur] != 0)
+		cur++;
+	return (cur);
+}
+
+char	*rev_str(char *str)
+{
+	int		size;
+	int		cur;
+	char	tmp;
+
+	size = ft_strlen(str) - 1;
+	cur = 0;
+	while (cur < size - cur)
+	{
+		tmp = str[cur];
+		str[cur] = str[size - cur];
+		str[size - cur] = tmp;
+		cur++;
+	}
+	return (str);
+}
+
+int	pos_in_str(char *str, char c)
+{
+	int	cur;
+
+	cur = 0;
+	while (str[cur] != 0)
+	{
+		if (str[cur] == c)
+			return (cur);
+		cur++;
+	}
+	return (-1);
+}
+
+////
 int	ft_is_in_base(char *base, char c)
 {
 	int	i;
