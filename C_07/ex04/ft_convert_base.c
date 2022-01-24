f0603c3cbbf52ae2f6843ebecece93e9b9bfc00b
33bini <68188768+33bini@users.noreply.github.com>
2022-01-25 00:43:02 +0900

    change

--- C_07/ex04/ft_convert_base.c
+++ C_07/ex04/ft_convert_base.c
@@ -12,6 +12,20 @@
 
 #include <stdlib.h>
 
+int	ft_malloc(char **nbr_str, int *is_neg, long *nbrl)
+{
+	*nbr_str = (char *)malloc(sizeof(char) * 33);
+	if (*nbr_str == NULL)
+		return (0);
+	*is_neg = 1;
+	if (*nbrl < 0)
+	{
+		*nbrl *= -1;
+		*is_neg = -1;
+	}
+	return (1);
+}
+
 int	ft_base_valid(char *base)
 {
 	int	i;
@@ -57,6 +71,36 @@ int	ft_str_base_len(char *base)
 	return (i);
 }
 
+char	*ft_putnbr_base(int nbr, char *base)
+{
+	long	nbrl;
+	char	*nbr_str;
+	int		base_len;
+	int		cur;
+	int		is_neg;
+
+	nbrl = nbr;
+	if (ft_malloc(&nbr_str, &is_neg, &nbrl) == 0)
+		return (0);
+	base_len = ft_strlen(base);
+	cur = 0;
+	while (nbrl > 0)
+	{
+		nbr_str[cur++] = base[nbrl % base_len];
+		nbrl /= base_len;
+	}
+	if (nbr == 0)
+	{
+		cur = 1;
+		nbr_str[0] = '0';
+	}
+	if (is_neg == -1)
+		nbr_str[cur++] = '-';
+	nbr_str[cur] = 0;
+	return (rev_str(nbr_str));
+}
+
+
 void	ft_putnbr_base(int nbr, char *base)
 {
 	int		len;
@@ -77,8 +121,14 @@ void	ft_putnbr_base(int nbr, char *base)
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int from ;
+	int		str_to_int;
+	char	*int_to_str;
 
-	from = 
-	return (nbr);
+	if (ft_check_base(base_from) == 0)
+		return (0);
+	if (ft_check_base(base_to) == 0)
+		return (0);
+	str_to_int = ft_atoi_base(nbr, base_from);
+	int_to_str = ft_putnbr_base(str_to_int, base_to);
+	return (int_to_str);
 }
