22c1a555aa8e678be54bd86a9e9825931b8a5b36
33bini <68188768+33bini@users.noreply.github.com>
2022-01-27 00:46:10 +0900

    Update ft_advanced_sort_string_tab.c

--- C_11/ex07/ft_advanced_sort_string_tab.c
+++ C_11/ex07/ft_advanced_sort_string_tab.c
@@ -6,29 +6,19 @@ void ft_swap(char *s1, char *s2)
     *s2 = temp;
 }
 
-int ft_cmp(char *s1, char *s2)
-{
-    int i;
-
-    i = 0;
-    while (s1[i] && s1[i] == s2[i])
-        i++;
-    return (s1[i] - s2[i]);
-}
 void ft_advanced_sort_string_tab(char **tab, int(*cmp)(char *, char *))
 {
     int i;
     int j;
 
     i = 0;
-    cmp = ft_cmp;
-    while (tab[i])
+    while (tab[i + 1])
     {
-        j = i + 1;
-        while(tab[j])
+        j = 0;
+        while(tab[j + 1 + i])
         {
-            if(cmp(tab[i], tab[j]) > 0)
-                ft_swap(tab[i], tab[j]);
+            if(cmp(tab[j], tab[j + 1]) > 0)
+                ft_swap(tab[j], tab[j + 1]);
             j++;
         }
         i++;
